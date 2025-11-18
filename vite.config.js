741861d75e6e872000a7c8c5fb69371104ffa016
RajatRawat10 <rajatrawat4242@gmail.com>
2025-11-18 17:01:27 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-   base:"/Weather-App-React/docs-mini-reactjs"
+   base:"/Weather-App-React/Doc-mini-Reactjs-"
 })
